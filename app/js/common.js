3ebb789e87bf2abe13c6611600fd9ac883d6cbff
ab <ytkonaft@gmail.com>
2020-04-12 23:27:11 +0300

    add navigation menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,16 @@
-$(function () {});
+// Получаем ссылку на кнопку и записываем её в переменную navigationButton
+const navigationButton = document.getElementById("navigation-toggle-btn");
+const navigationMenu = document.getElementById("navigation-menu");
+
+// Добавляем обработчик событий на 'click' по кнопке "#navigation-toggle-btn" ссылка которой находится в переменной navigationButton
+navigationButton.addEventListener("click", function() {
+  // Удаляем класс при нажатии на кнопку
+  // navigationButton.classList.remove("active");
+
+  // Добавляем класс при нажатии на кнопку
+  // navigationButton.classList.add("active");
+
+  // Добавляем класс при нажатии на кнопку и удаляем его при повторном нажатии
+  navigationButton.classList.toggle("active");
+  navigationMenu.classList.toggle("active");
+});
